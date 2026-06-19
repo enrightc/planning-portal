1fa2f0443b8bed8c07ed52b1c6606c1183ee8dc6
Charlie <c.enright@me.com>
2026-06-19 15:37:17 +0100

    Feat: Add leafet draw

--- map.js
+++ map.js
@@ -4,3 +4,16 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+// leaflet draw
+var drawControl = new L.Control.Draw();
+map.addControl(drawControl);
+
+var drawnFeatures = new L.FeatureGroup();
+map.addLayer(drawnFeatures);
+
+map.on("draw:created", function (e) {
+   var type = e.layerType,
+       layer = e.layer;
+   drawnFeatures.addLayer(layer);
+});
